df695f34271a4e934842d9928d33305eb1ed35f9
YourGitHubUsername <your-email@example.com>
2025-03-18 16:51:57 -0700

    print method

---
+++ PrintMethods.java
@@ -0,0 +1,20 @@
+public class PrintMethods{
+	public static void main (String[] args){
+		//using the print()
+		System.out.print("\"Learning Java is fun\"\n");
+		System.out.print("Hey, come and learn Java with me");
+		
+		//Using the println()
+		System.out.println("");
+		System.out.println("I want to tell you that Java is different from Javascript");
+		System.out.println("\t This is an indented text");
+		
+		//Using the printf()
+		System.out.printf("My name is %s and i love %s%n ","John","Java");
+		System. out.printf("I am living in No.%d Kaduna street %n",1);
+		System.out.printf("I bought Java JDK with %c%f.2",'$',159.345);
+		System.out.printf("I bought Java JDK with %.2f Naira%n",159.345);
+		
+		
+	}	
+}
