c0d0f91d3140bee100ab90140d4c568b84411fde
YourGitHubUsername <your-email@example.com>
2025-03-20 17:06:20 -0700

    UserInput

---
+++ TypeCasting.java
@@ -0,0 +1,18 @@
+public class TypeCasting{
+	public static void main(String[] args) {
+		//Widening Type Casting 
+		double weight = 678;
+		System.out.printf("Your weight is %f%n",weight)
+		
+		//Narrowing Type Casting ASCII Code ? = 63
+		char alpha = '?'
+		
+		int asciiCode = (char)alpha;
+		System.out.printf(The ASCII Code for ? is %d$n",asciicode);
+		
+		double height = 5.8;
+		
+		int myHeight = (int)height,
+		System.out.printf("My faculty's height is %d",myHeight);
+	}
+}
